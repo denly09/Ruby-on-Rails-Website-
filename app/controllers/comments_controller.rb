b1bc757abc2d30fb39f46445cfa56d5df91b1c28
Denly09 <denniely.garcia09@gmail.com>
2022-10-25 12:05:37 +0800

    post comments index action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
+  def index
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_post
+    @post = Post.find params[:post_id]
+  end
 end
